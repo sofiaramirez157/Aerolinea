9d97216764c037734e229dc3cf4baadb9e4eb427
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-09-24 17:58:13 +0200

    finished all methods

--- src/main/java/com/example/Aerolinea/controller/FlightController.java
+++ src/main/java/com/example/Aerolinea/controller/FlightController.java
@@ -1,4 +1,48 @@
 package com.example.Aerolinea.controller;
 
+import com.example.Aerolinea.model.Flight;
+import com.example.Aerolinea.service.FlightService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/flight")
+@CrossOrigin
+
 public class FlightController {
+    @Autowired
+    FlightService flightService;
+
+    @PostMapping(path = "post")
+    public Flight createFlight(@RequestBody Flight flight) {
+        return flightService.createFlight(flight);
+    }
+
+    @GetMapping(path = "get")
+    public List<Flight> getAllFlight() {
+        return flightService.getAllFlight();
+    }
+
+    @GetMapping(path = "get/{id}")
+    public Optional<Flight> getFlightById(@PathVariable long id) {
+        return flightService.getFlightById(id);
+    }
+
+    @PutMapping(path = "put/{id}")
+    public void updateFlight(@RequestBody Flight flight, @PathVariable long id) {
+        flightService.updateFlight(flight, id);
+    }
+
+    @DeleteMapping(path = "delete/{id}")
+    public String deleteFlightById(@PathVariable long id) {
+        boolean ok = flightService.deleteFlight(id);
+        if (ok) {
+            return "Flight with id" + id + "was deleted";
+        } else {
+            return "Destination with id" + id + "not found";
+        }
+    }
 }
--- src/main/java/com/example/Aerolinea/service/FlightService.java
+++ src/main/java/com/example/Aerolinea/service/FlightService.java
@@ -1,4 +1,50 @@
 package com.example.Aerolinea.service;
 
+import com.example.Aerolinea.repositories.IFlightRepository;
+import com.example.Aerolinea.model.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FlightService {
+    @Autowired IFlightRepository iFlightRepository;
+
+    public Flight createFlight(Flight newFlight) {
+        return iFlightRepository.save(newFlight);
+    }
+
+    public List<Flight> getAllFlight() {
+        try {
+            return iFlightRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving flights", e);
+        }
+    }
+
+    public Optional<Flight> getFlightById(long id) {
+        try {
+            return iFlightRepository.findById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving flight details", e);
+        }
+    }
+
+    public void updateFlight(Flight flight, long id) {
+        flight.setId(id);
+        iFlightRepository.save(flight);
+    }
+
+    public boolean deleteFlight(long id) {
+        try {
+            iFlightRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
 }
