5d205b5a85dc46a67cdcaf0a1a97a42539120cc4
IsabelGV1985 <isabelmgutierrez@hotmail.es>
2024-09-24 14:31:12 +0200

    added UserDto

--- src/main/java/com/example/Aerolinea/dto/UserDTO.java
+++ src/main/java/com/example/Aerolinea/dto/UserDTO.java
@@ -1,4 +1,38 @@
-package com.example.Aerolinea.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String email;
+    private List<Role> roles;
+
+
+    public static UserDTO fromUser(User user) {
+        return new UserDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRoles()
+        );
+    }
+
+
+    public static User toUser(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setRoles(userDTO.getRoles());
+        return user;
+    }
 }
+
+
