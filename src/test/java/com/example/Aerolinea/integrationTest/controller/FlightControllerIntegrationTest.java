8a26ab9de06b587392f0b8adf8ee203323a687c2
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-10-03 15:24:16 +0200

    added some services test

--- src/test/java/com/example/Aerolinea/integrationTest/controller/FlightControllerIntegrationTest.java
+++ src/test/java/com/example/Aerolinea/integrationTest/controller/FlightControllerIntegrationTest.java
@@ -7,8 +7,6 @@ import com.example.Aerolinea.service.FlightService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -21,7 +19,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
---
+++ src/test/java/com/example/Aerolinea/integrationTest/service/FlightServiceIntegrationTest.java
@@ -0,0 +1,91 @@
+package com.example.Aerolinea.integrationTest.service;
+
+import com.example.Aerolinea.model.Flight;
+import com.example.Aerolinea.repositories.IFlightRepository;
+import com.example.Aerolinea.service.FlightService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class FlightServiceIntegrationTest {
+    @Mock private IFlightRepository iFlightRepository;
+    @InjectMocks private FlightService flightService;
+
+    private Flight flight1;
+    private Flight flight2;
+    private List<Flight> flightList = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        flight1 = new Flight();
+        flight1.setId(1);
+        flight1.setOrigin("Madrid");
+        flight1.setDepartureTime(LocalTime.of(8, 15));
+        flight1.setArrivalTime(LocalTime.of(10, 20));
+        flight1.setAvailableSeats(60);
+        flight1.setStatus(true);
+
+        flight2 = new Flight();
+        flight2.setId(2);
+        flight2.setOrigin("Scotland");
+        flight2.setDepartureTime(LocalTime.of(15, 00));
+        flight2.setArrivalTime(LocalTime.of(20, 35));
+        flight2.setAvailableSeats(0);
+        flight2.setStatus(false);
+
+        flightList.add(flight1);
+        flightList.add(flight2);
+
+    }
+
+    @Test
+    void createFlight() {
+        when(iFlightRepository.save(ArgumentMatchers.any(Flight.class))).thenReturn(flight2);
+
+        Flight result = flightService.createFlight(flight2);
+        assertEquals(2, result.getId());
+        assertEquals("Scotland", result.getOrigin());
+        assertEquals(LocalTime.of(15, 00), result.getDepartureTime());
+        assertEquals(LocalTime.of(20, 35), result.getArrivalTime());
+        assertEquals(0, result.getAvailableSeats());
+        assertEquals(false, result.isStatus());
+    }
+
+    @Test
+    void getAllFlight() {
+        when(iFlightRepository.findAll()).thenReturn(flightList);
+
+        List<Flight> result = flightService.getAllFlight();
+
+        assertEquals(2, result.size());
+        assertEquals(1, result.get(0).getId());
+        assertEquals("Madrid", result.get(0).getOrigin());
+        assertEquals(LocalTime.of(8, 15), result.get(0).getDepartureTime());
+        assertEquals(LocalTime.of(10, 20), result.get(0).getArrivalTime());
+        assertEquals(60, result.get(0).getAvailableSeats());
+        assertEquals(true, result.get(0).isStatus());
+
+        assertEquals(2, result.get(1).getId());
+        assertEquals("Scotland", result.get(1).getOrigin());
+        assertEquals(LocalTime.of(15, 00), result.get(1).getDepartureTime());
+        assertEquals(LocalTime.of(20, 35), result.get(1).getArrivalTime());
+        assertEquals(0, result.get(1).getAvailableSeats());
+        assertEquals(false, result.get(1).isStatus());
+    }
+
+    
+
+}
